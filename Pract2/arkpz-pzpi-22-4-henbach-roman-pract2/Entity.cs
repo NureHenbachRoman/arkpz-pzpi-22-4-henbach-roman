63f2ca80ecfd0589dd85458dde759d0f80fe5c50
NureHenbachRoman <roman.henbach@nure.ua>
2024-11-18 12:32:58 +0300

    Upload code examples for Pract2

---
+++ Pract2/arkpz-pzpi-22-4-henbach-roman-pract2/Entity.cs
@@ -0,0 +1,32 @@
+﻿using Battle_City.Fields;
+using Battle_City.Internal_Code;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static Battle_City.Internal_Code.Globals;
+
+namespace Battle_City.Game_Elements.Entities
+{
+    public abstract class Entity : Visible
+    {
+        public bool IsDead { get; private set; }
+        protected EntitiesField entitiesField;
+
+        public Entity(int x, int y, int width, int height, EntitiesField entitiesField) 
+            : base(x, y, width, height)
+        {
+            this.entitiesField = entitiesField;
+            this.entitiesField.PlaceOnField(this);
+        }
+
+        public virtual void Die()
+        {
+            IsDead = true;
+            entitiesField.Remove(this);
+        }
+    }
+}
